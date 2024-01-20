fd946dba0dea2553a20950fce4045b1200787778
Young K. Jun <young.k.jun@gmail.com>
2024-01-20 16:54:49 -0600

    Best Time to Buy and Sell Stock II

---
+++ src/main/java/com/wemaxsoft/algorithm/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,25 @@
+package com.wemaxsoft.algorithm;
+
+public class BestTimeToBuyAndSellStock2 {
+
+	public static void main(String[] args) {
+		BestTimeToBuyAndSellStock2 test = new BestTimeToBuyAndSellStock2();
+		
+		int[] prices = new int[] {7,1,5,3,6,4};
+		int profit = test.maxProfit(prices);
+		
+		System.out.println(profit);
+	}
+	
+	public int maxProfit(int[] prices) {
+        int profit = 0;
+        
+        for (int i=0; i<prices.length-1; i++) {
+        	if (prices[i] < prices[i+1]){
+                profit += prices[i+1] - prices[i];
+            }
+        }
+        
+        return profit;
+    }
+}
